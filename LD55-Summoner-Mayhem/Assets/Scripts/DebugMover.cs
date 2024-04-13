08ecbf8584bb6fbbc8621c3e8047893aeb4f3f5f
ilmari21 <juuso.u67@gmail.com>
2024-04-13 12:41:48 +0300

    .

--- LD55-Summoner-Mayhem/Assets/Scripts/DebugMover.cs
+++ LD55-Summoner-Mayhem/Assets/Scripts/DebugMover.cs
@@ -4,15 +4,26 @@ using UnityEngine;
 
 public class DebugMover : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    public GameObject character;
+
+    [SerializeField]
+    float speed;
+
+    void Awake()
     {
         
     }
 
-    // Update is called once per frame
-    void Update()
+    void FixedUpdate()
     {
-        
+        {
+            float horizontal = Input.GetAxis("Horizontal");
+            var deltaH = speed * horizontal * Vector3.right * Time.deltaTime;
+            transform.position += deltaH;
+
+            float vertical = Input.GetAxis("Vertical");
+            var deltaV = speed * vertical * Vector3.up * Time.deltaTime;
+            transform.position += deltaV;
+        }
     }
 }
