6a32e47ac4a99beb075b3b9df57dce7310627aa7
larseemil <148450845+larseemil@users.noreply.github.com>
2024-04-13 17:49:28 +0300

    Added enemy attack

---
+++ LD55-Summoner-Mayhem/Assets/Scripts/MinionAttack.cs
@@ -0,0 +1,17 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MinionAttack : MonoBehaviour
+{
+    [SerializeField] int damage;
+
+    void OnCollisionEnter2D(Collision2D coll) {
+        if (coll.gameObject.GetComponent<IDamageable>() != null) {
+            var enemy = coll.gameObject.GetComponent<IDamageable>();
+            print("damage");
+            enemy.Damage(damage);
+        }
+    }
+
+}
