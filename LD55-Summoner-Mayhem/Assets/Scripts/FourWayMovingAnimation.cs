e3e6b47adb9b07dce31768e3f9d82e6e4d58d340
ilmari21 <juuso.u67@gmail.com>
2024-05-08 13:40:12 +0300

    enemyanims

---
+++ LD55-Summoner-Mayhem/Assets/Scripts/FourWayMovingAnimation.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class FourWayMovingAnimation : MonoBehaviour {
+	int lastAnimSector;
+
+	public Animator animator;
+
+    string[] movingAnimations =
+        new string[] {"Minion1_North", "Minion1_East", "Minion1_South", "Minion1_West" };
+
+    void Awake () {
+		animator = GetComponentInChildren<Animator>();
+	}
+	
+	void Update () {
+		var d = transform.forward;
+		float shortestAngle = Vector3.Angle(Vector3.up, d);
+		float clockwiseAngle = d.x >= 0 ? shortestAngle : 360 - shortestAngle;
+		int sector = ((int)(clockwiseAngle + 45f) % 360) / 90;
+
+		if (sector != lastAnimSector)
+		{
+			print(sector);
+			animator.Play(movingAnimations[sector]);
+		}
+
+		lastAnimSector = sector;
+	}
+}
--- LD55-Summoner-Mayhem/Assets/Scripts/SpriteOrientation.cs
+++ LD55-Summoner-Mayhem/Assets/Scripts/SpriteOrientation.cs
@@ -4,28 +4,8 @@ using UnityEngine;
 
 public class SpriteOrientation : MonoBehaviour
 {
-    public Animator animator;
-    public SpriteRenderer spriteRend;
-
-    void Awake()
-    {
-        animator = GetComponent<Animator>();
-        spriteRend = GetComponent<SpriteRenderer>();
-        print(spriteRend);
-    }
-
     void Update()
     {
         transform.rotation = Quaternion.identity;
-        if (transform.rotation.z >= 0)
-        {
-            print(transform.rotation.z);
-            SetSpriteFlip(true);
-        }
-    }
-
-    void SetSpriteFlip(bool flipped)
-    {
-        spriteRend.flipX = flipped;
     }
 }
