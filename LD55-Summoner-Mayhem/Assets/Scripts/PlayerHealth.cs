17e81c00a3d33bef48e13fdf5625f0576ebbf2c5
larseemil <148450845+larseemil@users.noreply.github.com>
2024-05-15 18:36:12 +0300

    Added player sprite flicker

--- LD55-Summoner-Mayhem/Assets/Scripts/PlayerHealth.cs
+++ LD55-Summoner-Mayhem/Assets/Scripts/PlayerHealth.cs
@@ -12,12 +12,16 @@ public class PlayerHealth : MonoBehaviour, IDamageable
     [SerializeField] float invDuration;
     float timer = 0f;
     [SerializeField] Slider healthSlider;
+    [SerializeField] Material hitMat;
+    [SerializeField] float flickerTimer;
+    bool flickerStarted;
 
     void Start()
     {
         manager = FindObjectOfType<GameManager>();
         health = defaultHealth;
         playerCon = GetComponent<PlayerController>();
+        hitMat.SetFloat("_hit", 0f);
     }
 
     void Update()
@@ -27,10 +31,28 @@ public class PlayerHealth : MonoBehaviour, IDamageable
         }
         if (playerCon.invulnerable == true) {
             timer += Time.deltaTime;
+            if (flickerStarted == false) {
+                StartFlicker();
+            }
             if (timer >= invDuration) {
                 playerCon.invulnerable = false;
+                hitMat.SetFloat("_hit", 0f);
+                flickerStarted = false;
                 timer = 0f;
             }
+        }
+    }
+
+    void StartFlicker() {
+        flickerStarted = true;
+        hitMat.SetFloat("_hit", 1.1f);
+        Invoke("EndFlicker", flickerTimer);
+    }
+
+    void EndFlicker() {
+        hitMat.SetFloat("_hit", 0f);
+        if (playerCon.invulnerable == true) {
+            Invoke("StartFlicker", flickerTimer);
         }
     }
 
